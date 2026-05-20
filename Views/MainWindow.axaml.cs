abf770159ff1c8541934e4193ec8e5d581dea0bc
Lars Oste <lars-oste@gmx.de>
2026-05-20 23:55:17 +0200

    feat: Hinzufügen einer benutzerdefinierten Titelleiste mit Minimize-, Maximize- und Close-Buttons

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using DTM.ViewModels;
 
 namespace DTM.Views;
@@ -14,4 +17,32 @@ public partial class MainWindow : Window
                 await vm.CheckForUpdateAsync();
         };
     }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+        if (change.Property == WindowStateProperty)
+            MaximizeButton.Content = WindowState == WindowState.Maximized ? "❐" : "☐";
+    }
+
+    private void OnTitleBarPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            BeginMoveDrag(e);
+    }
+
+    private void OnTitleBarDoubleTapped(object? sender, TappedEventArgs e) =>
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
+
+    private void OnMinimize(object? _, RoutedEventArgs e) =>
+        WindowState = WindowState.Minimized;
+
+    private void OnMaximize(object? _, RoutedEventArgs e) =>
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
+
+    private void OnClose(object? _, RoutedEventArgs e) => Close();
 }
